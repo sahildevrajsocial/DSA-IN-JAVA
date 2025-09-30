073e73d0e99e69ef76b6a5cdd5eb60c95cb8844b
sahildevrajsocial <sdevrajsocial@gmail.com>
2025-09-30 23:46:21 +0530

    init commit

---
+++ com/devraj/strings/StringsVeryBasic.java
@@ -0,0 +1,11 @@
+package com.devraj.strings;
+
+public class StringsVeryBasic {
+    public static void main(String args[]) {
+
+        // creating Java string using a new keyword
+        String str = new String("Geeks");
+
+        System.out.println(str);
+    }
+}
