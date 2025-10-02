b4ef90a7ffe02cb847eeda5e7309303cd340fa45
sahildevrajsocial <sdevrajsocial@gmail.com>
2025-10-02 23:56:21 +0530

    init commit

---
+++ com/devraj/array/BasicArray.java
@@ -0,0 +1,17 @@
+package com.devraj.array;
+
+public class BasicArray {
+    public static void main(String[] args)
+    {
+
+        // initializing array
+        int[] arr = {40, 55, 63, 17, 22};
+
+        // size of array
+        int n = arr.length;
+
+        // traversing array
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+}
