8d67671cf2e5d17f6f059a6fe1b12613a9191149
Sahil Devraj <sdevrajsocial@gmail.com>
2025-11-21 11:41:35 +0530

    init commit

---
+++ com/devraj/array/MinimumAverageSubarray.java
@@ -0,0 +1,31 @@
+package com.devraj.array;
+
+public class MinimumAverageSubarray {
+    public static void main(String[] args) {
+        int[] arr = {10,2,3,5,9,1,4};
+        int target = 2;
+        int sum = 0;
+
+        for(int i = 0; i<target; i++) {
+            sum += arr[i];
+        }
+
+        double minAverage = (double) sum/target;
+        int left = 0;
+        int right = target - 1;
+        double sumAverage2;
+
+
+        while(right < arr.length-1) {
+
+            sum = sum - arr[left];
+            left++;
+            right++;
+            sum = sum + arr[right];
+            sumAverage2 = (double) sum/target;
+            minAverage = Math.min(minAverage, sumAverage2);
+        }
+        System.out.println(minAverage);
+        System.out.println(arr[left]+","+arr[right]);
+    }
+}
